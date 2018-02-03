15d2a3362a361687ed27b6c65b6a8f937870dc09
austinoboyle <14arob@queensu.ca>
2018-02-03 12:59:54 -0500

    Add facebook login using react-facebook-login component

---
+++ src/auth/FacebookLoginButton.js
@@ -0,0 +1,68 @@
+'use strict';
+
+import React, {Component} from 'react';
+import FacebookLogin from 'react-facebook-login';
+
+/**
+ * 
+ * 
+ * @class FacebookLoginButton
+ * @extends {Component}
+ */
+class FacebookLoginButton extends Component {
+
+  constructor(props) {
+    super(props);
+    this.responseFacebook = this.responseFacebook.bind(this);
+  }
+
+  responseFacebook(response){
+    console.log('FACEBOOK AUTH', response);
+  }
+
+  componentDidMount(){
+    
+  }
+
+  render(){
+    window.fbAsyncInit = function() {
+      window.FB.init({
+        appID   : '926916724132817',
+        cookie  : true,
+        xfbml   : true,
+        version : 'v2.11'
+      });
+      console.log('Asyn Executed')
+      window.FB.getLoginStatus(function(response) {
+        console.log(response)
+      });
+    }.bind(this);
+
+    (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId=926916724132817";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+    if(window.FB){
+      window.FB.getLoginStatus(function(response) {
+        console.log('Response:', response)
+      });
+    }
+  return(
+    <FacebookLogin
+        appID="926916724132817"
+        // autoLoad={true}
+        size="small"
+        xfbml={true}
+        cookie={true}
+        version='2.11'
+        scope='email'
+        fields='name,email'
+        callback={this.responseFacebook}/>
+  )
+}
+}
+
+export default FacebookLoginButton;
--- src/auth/LoginForm.js
+++ src/auth/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import FacebookLogin from 'react-facebook-login';
 import {
   Button,
   Form,
@@ -8,6 +9,7 @@ import {
   Message,
   Segment
 } from 'semantic-ui-react';
+import FacebookLoginButton from './FacebookLoginButton';
 
 export default class LoginForm extends Component {
   constructor(props) {
@@ -17,18 +19,14 @@ export default class LoginForm extends Component {
   render() {
     return (
       <div className="login-form">
-        {/*
-          Heads up! The styles below are necessary for the correct render of this example.
-          You can do same with CSS, the main idea is that all the elements up to the `Grid`
-          below must have a height of 100%.
-        */}
-        <style>{`
-      body > div,
-      body > div > div,
-      body > div > div > div.login-form {
-        height: 100%;
-      }
-    `}</style>
+    <style>{`
+        body > div,
+        body > div > div,
+        body > div > div > div.login-form {
+          height: 100%;
+        }
+      `}
+    </style>
         <Grid
           textAlign="center"
           style={{ height: '100%' }}
@@ -39,25 +37,7 @@ export default class LoginForm extends Component {
               Login to QCourses
             </Header>
             <Form size="large">
-              <Segment stacked>
-                <Form.Input
-                  fluid
-                  icon="user"
-                  iconPosition="left"
-                  placeholder="E-mail address"
-                />
-                <Form.Input
-                  fluid
-                  icon="lock"
-                  iconPosition="left"
-                  placeholder="Password"
-                  type="password"
-                />
-
-                <Button color="teal" fluid size="large">
-                  Login
-                </Button>
-              </Segment>
+                <FacebookLoginButton/>
             </Form>
             <Message>
               New user? <a href="#">Sign Up</a>
