e2c5677553dc2fcb328eed1f75a1b6d6103ead9b
Shaun Bennett <me@phinary.ca>
2018-02-03 17:50:03 -0500

    Browse page

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import {
 
 import SignupForm from './auth/SignupForm';
 import LoginForm from './auth/LoginForm';
+import Browse from './Browse';
 
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
@@ -66,6 +67,7 @@ class App extends Component {
           </Menu>
 
           <SignupForm />
+          <Browse />
           {/* <LoginForm /> */}
 
           {/* <Container text style={{ marginTop: "7em" }}>
